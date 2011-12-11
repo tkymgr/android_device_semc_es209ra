e99d246c7d6e52e82b638b3c7318b906f4547b37
Goro Tsukiyama <goro.tsukiyama@gmail.com>
2011-12-11 11:02:36 +0900

    change vendor files

--- extract-files.sh
+++ extract-files.sh
@@ -3,15 +3,15 @@
 VENDOR=semc
 DEVICE=es209ra
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+#BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+#rm -rf $BASE/*
 
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
-done
+#for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+#    DIR=`dirname $FILE`
+#    if [ ! -d $BASE/$DIR ]; then
+#        mkdir -p $BASE/$DIR
+#    fi
+#    adb pull /system/$FILE $BASE/$FILE
+#done
 
 ./setup-makefiles.sh
